e67824903ddfe72a6c991e95d8ce60b9d8015e97
Gayathri Ravichandran <gayathrix7@gmail.com>
2018-06-13 12:44:36 +0530

    commit

---
+++ try_wiki_scrape.py
@@ -0,0 +1,41 @@
+import wikipedia
+import nltk
+from nltk import  word_tokenize
+import json
+
+
+topic_to_noun = {}
+def extract_nouns(content,topic):
+    text = word_tokenize(content)
+    text = nltk.pos_tag(text)
+    for word in text:
+        if word[1] == "NN":
+            if word[0].lower() not in topic_to_noun:
+                topic_to_noun[topic][word[0].lower()] = 1
+
+
+def scrape_data(topics):
+    for topic in topics:
+        topic_to_noun[topic] = {}
+        content = wikipedia.WikipediaPage(topic).content
+        extract_nouns(content,topic)
+        external_links = wikipedia.WikipediaPage(topic).links
+        for links in external_links:   #follow the links and extract content 
+            link_content = wikipedia.WikipediaPage(links).content
+            extract_nouns(link_content,topic)
+        print("--- Finished scraping topic! " + str(topic) + " ----")
+
+def writeToFile():
+    with open('wiki_extracts.txt', 'w') as outfile:
+        json.dump(topic_to_noun, outfile, indent=4)
+    print("Write complete!")
+
+
+if __name__ == '__main__':
+
+    list_of_topics = ["travel","computer","food","literature","politics"]
+    scrape_data(list_of_topics)
+    print("--------")
+    print("finished scraping all the topics!")
+    print("Let us write the result to a file!")
+    writeToFile()
